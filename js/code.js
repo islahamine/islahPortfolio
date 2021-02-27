2149b03463793dd682a1a330597d47c6547eccad
AMINE ISLAH <32944101+amine-islah@users.noreply.github.com>
2021-02-28 00:51:13 +0100

    My Personal Portfolio

---
+++ js/code.js
@@ -0,0 +1,17 @@
+$(window).on("load", function () {
+    'use strict';
+ /*   setInterval(function () {
+        $(".loading").fadeOut("slow",
+                    function () {
+                $(this).remove();
+                $(window).scrollTop(0);
+            }, 400);
+    });*/
+
+        setInterval(function(){
+               $("body").css("overflow-y","scroll");
+               $(".loading").fadeOut("slow",function () {
+                $(this).remove();
+                $(window).scrollTop(0);
+               });},2700);
+});
